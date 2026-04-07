28b19a3ac9b60f75fd30072547f341c66c8ffa35
layoutnova7 <vtutorias9@gmail.com>
2026-04-07 15:22:43 -0600

    4 modulos
    Modulos referentes a citas.

---
+++ HospitalWebAPI/ICitaRepository.cs
@@ -0,0 +1,8 @@
+﻿//Leonel Contreras-20191900210
+using HospitalWebAPI.Models;
+namespace HospitalWebAPI.Interfaces { public interface ICitaRepository { List<Cita> GetAll(); 
+        Cita GetById(int id); 
+        void Add(Cita cita); 
+        void Update(Cita cita); 
+        void Delete(int id); } 
+}
