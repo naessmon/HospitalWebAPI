d3317e5c5ecc110e813cf8054d0b991c01361053
nazadub18 <nazadub18@gmail.com>
2026-04-07 15:14:01 -0600

    Agregando MedicoService y controladores

---
+++ HospitalWebAPI/Services/MedicoService.cs
@@ -0,0 +1,37 @@
+﻿//NAZARETH DUBÓN
+
+using HospitalWebAPI.Interfaces;
+using HospitalWebAPI.Models;
+namespace HospitalWebAPI.Services
+{
+    public class MedicoService
+    {
+        private readonly IMedicoRepository _repository;
+        public MedicoService(IMedicoRepository repository)
+        {
+            _repository = repository;
+        }
+        public List<Medico> GetAll()
+        {
+            return _repository.GetAll();
+        }
+        public Medico GetById(int id)
+        {
+            return _repository.GetById(id); 
+        }
+        public void Create(Medico medico)
+        {
+            medico.Activo = true;
+            medico.FechaCreacion = DateTime.Now;
+            _repository.Add(medico); 
+        }
+        public void Update(Medico medico) 
+        {
+            _repository.Update(medico); 
+        }
+        public void Delete(int id)
+        {
+            _repository.Delete(id); 
+        }
+    }
+}
