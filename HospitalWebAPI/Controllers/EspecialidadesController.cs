cd4548d33a328a35644277e6ae5c17a6870b7d4c
Natalie Machado <machado.natalie17@gmail.com>
2026-04-07 22:41:36 -0600

    Subidas las partes de Wilhem

---
+++ HospitalWebAPI/Controllers/EspecialidadesController.cs
@@ -0,0 +1,61 @@
+﻿// Subida la parte de Wilhem
+using HospitalWebAPI.Models;
+using HospitalWebAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EspecialidadesController : ControllerBase
+    {
+        private readonly EspecialidadService _service;
+
+        public EspecialidadesController(EspecialidadService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_service.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var especialidad = _service.GetById(id);
+            if (especialidad == null)
+                return NotFound();
+
+            return Ok(especialidad);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Especialidad
+especialidad)
+        {
+            _service.Create(especialidad);
+            return Ok(especialidad);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] Especialidad
+especialidad)
+        {
+            if (id != especialidad.Id)
+                return BadRequest();
+
+            _service.Update(especialidad);
+            return Ok(especialidad);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _service.Delete(id);
+            return Ok();
+        }
+    }
+}
